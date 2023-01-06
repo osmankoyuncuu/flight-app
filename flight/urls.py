833e8d0ce42c6b58bbc8b9b2389129280392e526
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-06 15:33:54 +0300

    add fligth permission, serializer, urls and view

---
+++ flight/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework import routers
+from .views import FlightView, ReservationView
+
+router = routers.DefaultRouter()
+router.register("flights", FlightView)
+router.register("reservations", ReservationView)
+
+urlpatterns = [
+]
+urlpatterns += router.urls
